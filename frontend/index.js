b7d0f4266d1866ab991186d5ff6f58312ecd5577
mliu2003 <liumax2003@gmail.com>
2022-10-22 14:48:38 -0400

    separated frontend/middle tier

--- frontend/index.js
+++ frontend/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import InfoPage from './infoPage';
-import MiddleTier from './middleTier'
-
-// const express = require("express");
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on ${PORT}`);
-// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
